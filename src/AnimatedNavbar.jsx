a4a8d51776f7f4506d1c214f71af92ae0ba1dc8b
luvpatel016 <1501lpatel@gmail.com>
2025-08-19 04:59:02 -0400

    updated project section

--- src/AnimatedNavbar.jsx
+++ src/AnimatedNavbar.jsx
@@ -5,11 +5,11 @@ const AnimatedNavbar = () => {
   return (
     <div className="navbar">
       <h1>Hello, I'm Luv Patel 🧑🏽‍💻</h1>
-      <div className="nav-links">
-        <a href="#projects">Projects</a>
-        <a href="mailto:1501lpatel@gmail.com">Email</a>
-        <a href="https://www.linkedin.com/in/luv-patel-395267299" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-        <a href="https://github.com/luvpatel016" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <div className="button-container">
+        <a href="#projects" className="nav-btn">Projects</a>
+        <a href="mailto:1501lpatel@gmail.com" className="nav-btn">Email</a>
+        <a href="https://www.linkedin.com/in/luv-patel-395267299" className="nav-btn" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://github.com/luvpatel016" className="nav-btn" target="_blank" rel="noopener noreferrer">GitHub</a>
       </div>
     </div>
   );
